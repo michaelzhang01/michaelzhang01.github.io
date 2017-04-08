00ac99728671b77ea065774831bf41b599d9a465
Michael Zhang <michael_zhang@utexas.edu>
2017-04-07 22:14:47 -0500

    working version again

--- js/content.js
+++ js/content.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
-	$("#content").load("main.html"); 
 	$("#sidebar").load("sidebar.html"); 
-	
-	$("#main").click(function(){
-		$("#content").load("main.html");
-	});
-
-	$("#pub").click(function(){
-		$("#content").load("publications2.html");
-	});
-	
 });	
-
